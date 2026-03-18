a30720d4367ac03b7493f499b7a77921d68743f3
Nipun Jayasinghe <nipunprjay@gmail.com>
2026-03-18 12:22:46 +0530

    Initial Commit

---
+++ NurseryConnect/NurseryConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseryConnectApp.swift
+//  NurseryConnect
+//
+//  Created by Nipun Jayasinghe on 2026-03-18.
+//
+
+import SwiftUI
+
+@main
+struct NurseryConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
